74ce37f94abecb8d50912516310df0c6725ee01c
ronaldtricoche <ronald.tricoche@yale.edu>
2014-12-11 13:14:32 -0500

    making new button on home screen - ron

--- src/sra/com/ContactsDemo.java
+++ src/sra/com/ContactsDemo.java
@@ -7,6 +7,7 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -18,52 +19,64 @@ import android.widget.TextView;
 	private Button mBtnContacts;
 	private final int PICK = 1;
 	Button Save;
-	 EditText group_name;
-	 EditText current_date;
-	 EditText due_date;
-	 EditText num_attendees;
-	 TextView testPrint; 
+	EditText group_name;
+	EditText current_date;
+	EditText due_date;
+	EditText num_attendees;
+	TextView testPrint; 
 
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) { //set to home screen when app starts
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.welcome_screen);
-		
+
 	}
+
 	public void makeNewGroup(View view){ // from the home screen when you click on Make New Group Button
 		setContentView(R.layout.new_group);
 	}
 	
+	Button group = null;
+
+	group.setOnClickListener(
+			new View.OnClickListener()
+			{
+				public void onClick(View view)
+				{
+					setContentView(R.layout.group);
+				}
+			});
+
 	public void saveGroup(View view){
-	    
+
 		group_name = (EditText)findViewById(R.id.group_name);
-	    
-	    current_date = (EditText)findViewById(R.id.current_date);
-	    
-	    due_date = (EditText)findViewById(R.id.due_date);
-	    num_attendees = (EditText)findViewById(R.id.attendees);
-	    
-	    Save = (Button) findViewById(R.id.Save);
-	    
-	    
-	    Save.setOnClickListener(new View.OnClickListener()
-	    {  //turning input into String
-	    	public void onClick(View v) 
-	    	{
-	    		group_name = (EditText)findViewById(R.id.group_name);
-	    	    current_date = (EditText)findViewById(R.id.current_date); 
-	    	    due_date = (EditText)findViewById(R.id.due_date);
-	    	    num_attendees = (EditText)findViewById(R.id.attendees);
-	    	
-	    	    String GroupName = group_name.getText().toString();
-	            String CurrentDate = current_date.getText().toString();
-	            String DueDate = due_date.getText().toString();
-	            String NumAttendees = num_attendees.getText().toString();
-	            testPrint= (TextView)findViewById(R.id.printOut);
-	            testPrint.setText("Welcome " + GroupName);  //need to print this home screen
-	    	}
-	    });
+
+		current_date = (EditText)findViewById(R.id.current_date);
+
+		due_date = (EditText)findViewById(R.id.due_date);
+		num_attendees = (EditText)findViewById(R.id.attendees);
+
+		Save = (Button) findViewById(R.id.Save);
+
+
+		Save.setOnClickListener(new View.OnClickListener()
+		{  //turning input into String
+			public void onClick(View v) 
+			{
+				group_name = (EditText)findViewById(R.id.group_name);
+				current_date = (EditText)findViewById(R.id.current_date); 
+				due_date = (EditText)findViewById(R.id.due_date);
+				num_attendees = (EditText)findViewById(R.id.attendees);
+
+				String GroupName = group_name.getText().toString();
+				String CurrentDate = current_date.getText().toString();
+				String DueDate = due_date.getText().toString();
+				String NumAttendees = num_attendees.getText().toString();
+				testPrint= (TextView)findViewById(R.id.printOut);
+				testPrint.setText("Welcome " + GroupName);  //need to print this home screen
+			}
+		});
 		//setContentView(R.layout.add_tasks); (need to re-implement this to a new button)
 	}
 	public void contact(View v){
@@ -96,24 +109,24 @@ import android.widget.TextView;
 			}
 		break;
 		}
-		}
-					// TODO Whatever you want to do with the selected contact
-					// name.
-	
-					public static void fetchContacts() {
-						        String phoneNumber = null;
-						        String email = null;
-						        Uri CONTENT_URI = ContactsContract.Contacts.CONTENT_URI;
-						        String _ID = ContactsContract.Contacts._ID;
-						        String DISPLAY_NAME = ContactsContract.Contacts.DISPLAY_NAME;
-						        String HAS_PHONE_NUMBER = ContactsContract.Contacts.HAS_PHONE_NUMBER;
-						        Uri PhoneCONTENT_URI = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
-						        String Phone_CONTACT_ID = ContactsContract.CommonDataKinds.Phone.CONTACT_ID;
-						        String NUMBER = ContactsContract.CommonDataKinds.Phone.NUMBER;
-						        StringBuffer output = new StringBuffer();
-						        System.out.println(NUMBER);
-						        System.out.println(DISPLAY_NAME);
-						      /*  Cursor cursor = ContentResolver.query(CONTENT_URI, null,null, null, null); 
+	}
+	// TODO Whatever you want to do with the selected contact
+	// name.
+
+	public static void fetchContacts() {
+		String phoneNumber = null;
+		String email = null;
+		Uri CONTENT_URI = ContactsContract.Contacts.CONTENT_URI;
+		String _ID = ContactsContract.Contacts._ID;
+		String DISPLAY_NAME = ContactsContract.Contacts.DISPLAY_NAME;
+		String HAS_PHONE_NUMBER = ContactsContract.Contacts.HAS_PHONE_NUMBER;
+		Uri PhoneCONTENT_URI = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+		String Phone_CONTACT_ID = ContactsContract.CommonDataKinds.Phone.CONTACT_ID;
+		String NUMBER = ContactsContract.CommonDataKinds.Phone.NUMBER;
+		StringBuffer output = new StringBuffer();
+		System.out.println(NUMBER);
+		System.out.println(DISPLAY_NAME);
+		/*  Cursor cursor = ContentResolver.query(CONTENT_URI, null,null, null, null); 
 						        // Loop for every contact in the phone
 						        if (cursor.getCount() > 0) {
 						            while (cursor.moveToNext()) {
@@ -127,30 +140,44 @@ import android.widget.TextView;
 						                    while (phoneCursor.moveToNext()) {
 						                        phoneNumber = phoneCursor.getString(phoneCursor.getColumnIndex(NUMBER));
 						                        output.append("\n Phone number:" + phoneNumber);
-						                        */
-						                    
-						                
-						            
-						       
-					
-		}
-					
-					public void goHome(View view) {
-						setContentView(R.layout.add_tasks);
-						Button home;
-						home = (Button) findViewById(R.id.home);
-						
-						home.setOnClickListener(new View.OnClickListener() {
+		 */
 
-							@Override
-							public void onClick(View v) {
-								// TODO Auto-generated method stub
-								
-								setContentView(R.layout.welcome_screen);
-								
-							}
-					
-						
-					});
+
+
+
+
+	}
+
+	public void goHome(View view) {
+		setContentView(R.layout.add_tasks);
+		Button home;
+		home = (Button) findViewById(R.id.home);
+
+		home.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+
+				setContentView(R.layout.welcome_screen);
+
+			}
+
+
+		});
+
+		Button group = null;
+
+		group.setOnClickListener(
+				new View.OnClickListener()
+				{
+					public void onClick(View view)
+					{
+						setContentView(R.layout.group);
 					}
+				});
+
+
+
+	}
 }
