2b84d14756baf74f8933538bbbd748e088310121
erikahairston <erikahairston@gmail.com>
2014-12-16 20:42:12 -0500

    With Contacts

--- src/sra/com/ContactsDemo.java
+++ src/sra/com/ContactsDemo.java
@@ -2,20 +2,21 @@ package sra.com;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.telephony.SmsManager;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.parse.Parse;
-import com.parse.ParseObject;
-import com.parse.*;
 
 @SuppressWarnings("deprecation")
 @SuppressLint("NewApi") public class ContactsDemo extends Activity implements OnClickListener {
@@ -33,8 +34,7 @@ import com.parse.*;
 	public void onCreate(Bundle savedInstanceState) { //set to home screen when app starts
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.welcome_screen);
-		Parse.initialize(this, "PEzHL5eKDrwO3r2ZDSqfiolgB2Ofl6I65ZpemYBT", "eRNfiouDMrcpkuLX5UI0Wyl07F5xMT8GxlH7lLp0");
-		Parse.enableLocalDatastore(getApplicationContext());
+
 	}
 
 	public void makeNewGroup(View view){ // from the home screen when you click on Make New Group Button
@@ -44,8 +44,8 @@ import com.parse.*;
 	public void groupPage (View view)
 	{
 		setContentView(R.layout.group);
-	}
-			
+	}		
+	
 	public void Home (View view)
 	{
 		setContentView(R.layout.welcome_screen);
@@ -57,8 +57,8 @@ import com.parse.*;
 		current_date = (EditText)findViewById(R.id.current_date);
 		due_date = (EditText)findViewById(R.id.due_date);
 		num_attendees = (EditText)findViewById(R.id.attendees);
-
 		Save = (Button) findViewById(R.id.Save);
+
 		Save.setOnClickListener(new View.OnClickListener()
 		{
 
@@ -77,25 +77,16 @@ import com.parse.*;
 	            String CurrentDate = current_date.getText().toString();
 	            String DueDate = due_date.getText().toString();
 	            String NumAttendees = num_attendees.getText().toString();
-	            //testPrint= (TextView)findViewById(R.id.printOut);
-	            //testPrint.setText("Welcome " + GroupName);  //need to print this home screen
-	            
-	            
-	            ParseObject group = new ParseObject("Group");
-	            group.put("group_name", GroupName);
-	            group.put("current_date", CurrentDate);
-	            group.put("due_date", CurrentDate);
-	            group.put("num_attendees", NumAttendees);
-	            group.saveInBackground(); 
-	            
-	            
+	            testPrint= (TextView)findViewById(R.id.printOut);
+	            testPrint.setText("Welcome " + GroupName);  //need to print this home screen
 	            Button button = (Button) findViewById(R.id.Save);
 	            button.setText("Assign Tasks");
             }
             else
             {
                 //code for move to next screen 
-            	setContentView(R.layout.add_tasks);
+            	setContentView(R.layout.pick_contact_button_screen);
+            	
                 
             }
         }
@@ -106,11 +97,15 @@ import com.parse.*;
 
 		//setContentView(R.layout.add_tasks); (need to re-implement this to a new button)
 	
-
+	
 	public void contact(View v){
-		setContentView(R.layout.pick_contact_button_screen);
+		//setContentView(R.layout.pick_contact_button_screen);
 		mBtnContacts = (Button) findViewById(R.id.xBtnContacts);
 		mBtnContacts.setOnClickListener(this);
+		Intent intent = new Intent(Intent.ACTION_PICK,
+				ContactsContract.Contacts.CONTENT_URI);
+		startActivityForResult(intent, PICK);
+		
 	}
 
 	@Override
@@ -119,63 +114,114 @@ import com.parse.*;
 		// Opening Contacts Window as a Window
 		Intent intent = new Intent(Intent.ACTION_PICK,
 				ContactsContract.Contacts.CONTENT_URI);
-		// calling OnActivityResult with intenet And Some conatct for Identifie
+		// calling OnActivityResult with intent And Some contact for identifier
 		startActivityForResult(intent, PICK);
 	}
-
+	
 	@Override
 	public void onActivityResult(int reqCode, int resultCode, Intent data) {
 		super.onActivityResult(reqCode, resultCode, data);
+        String phone = "";
+        final String PREFS_NAME = "MyApp_Settings";
 		switch (reqCode) {
+		//what is called when you select a contact
 		case (PICK):
 			if (resultCode == Activity.RESULT_OK) {
 				Uri contactData = data.getData();
 				Cursor c = managedQuery(contactData, null, null, null, null);
+	            ContentResolver cr = getContentResolver();
 				if (c.moveToFirst()) {
-					fetchContacts();
+						
+					try
+	                 {
+						//getting the display name to read into String name
+	                     String id = c.getString(c.getColumnIndex(ContactsContract.Contacts._ID));
+	                     String name = c.getString(c.getColumnIndexOrThrow(ContactsContract.Contacts.DISPLAY_NAME));
+	                     SharedPreferences settings = getApplicationContext().getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+	                     SharedPreferences.Editor editor = settings.edit();
+	                     //stores string
+	                     String GroupA = "Group A:";
+	                     editor.putString(GroupA, name);
+	                     //Apply the edits!
+	                     editor.apply();
+	                    //access stored string
+	                     String name1 = settings.getString(GroupA, "");
+	                     String name2 = settings.getString("", "");
+	                     name2 = (name1 + "" + name2);
+	                     editor.putString("name2:", name2);
+	                     //Apply the edits!
+	                     editor.apply();
+
+	                     
+	                     if (Integer.parseInt(c.getString(c.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))) > 0) 
+	                     {
+	                        Cursor pCur = cr.query(
+	                        ContactsContract.CommonDataKinds.Phone.CONTENT_URI, 
+	                        null, 
+	                        ContactsContract.CommonDataKinds.Phone.CONTACT_ID +" = ?", 
+	                        new String[]{id}, null);
+	                        while (pCur.moveToNext()) 
+	                        {
+	                            try
+	                            {
+	                            	//getting the actual phone number to read into String phone
+	                                phone = phone + pCur.getString(pCur.getColumnIndex(ContactsContract.Contacts.Data.DATA1)) + ",";
+	                            }
+	                            catch(Exception ex)
+	                            {
+	        	 			       	Toast.makeText(getApplicationContext(), "No phone number listed", Toast.LENGTH_LONG).show();
+
+	                            }
+	                        } 
+	                        pCur.close();
+	                        if(phone.length() > 0)
+	                        {
+	                        	//getting rid of the comma on the end
+	                            phone = phone.substring(0,phone.length()-1);
+	   	                     	//stores string
+	   	                     	editor.putString("number:", phone);
+	   	                     	// Apply the edits!
+	   	                     	editor.apply();
+	   	                     	//access stored string
+	   	                     	String number = settings.getString("number:", "");
+	   	                     	System.out.println(number);
+	                        }
+	                     }
+	                 }
+	                 catch(Exception ex)
+	                 {
+	 			       	Toast.makeText(getApplicationContext(), "No name selected", Toast.LENGTH_LONG).show();
+	                 }
+	             }
+				        String message = "Hi there!";
+			        
+					     SMS(phone, message);
+
+			      }
+			   }
+				                    
 				}
-			}
-		break;
-		}
-	}
-	// TODO Whatever you want to do with the selected contact
-	// name.
-
-	public static void fetchContacts() {
-		String phoneNumber = null;
-		String email = null;
-		Uri CONTENT_URI = ContactsContract.Contacts.CONTENT_URI;
-		String _ID = ContactsContract.Contacts._ID;
-		String DISPLAY_NAME = ContactsContract.Contacts.DISPLAY_NAME;
-		String HAS_PHONE_NUMBER = ContactsContract.Contacts.HAS_PHONE_NUMBER;
-		Uri PhoneCONTENT_URI = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
-		String Phone_CONTACT_ID = ContactsContract.CommonDataKinds.Phone.CONTACT_ID;
-		String NUMBER = ContactsContract.CommonDataKinds.Phone.NUMBER;
-		StringBuffer output = new StringBuffer();
-		System.out.println(NUMBER);
-		System.out.println(DISPLAY_NAME);
-		/*  Cursor cursor = ContentResolver.query(CONTENT_URI, null,null, null, null); 
-						        // Loop for every contact in the phone
-						        if (cursor.getCount() > 0) {
-						            while (cursor.moveToNext()) {
-						                String contact_id = cursor.getString(cursor.getColumnIndex( _ID ));
-						                String name = cursor.getString(cursor.getColumnIndex( DISPLAY_NAME ));
-						                int hasPhoneNumber = Integer.parseInt(cursor.getString(cursor.getColumnIndex( HAS_PHONE_NUMBER )));
-						                if (hasPhoneNumber > 0) {
-						                    output.append("\n First Name:" + name);
-						                    // Query and loop for every phone number of the contact
-						                    Cursor phoneCursor = contentResolver.query(PhoneCONTENT_URI, null, Phone_CONTACT_ID + " = ?", new String[] { contact_id }, null);
-						                    while (phoneCursor.moveToNext()) {
-						                        phoneNumber = phoneCursor.getString(phoneCursor.getColumnIndex(NUMBER));
-						                        output.append("\n Phone number:" + phoneNumber);
-
-		 */
+	
 
 
 
-
-
-	}
+public void SMS (String phone, String message){
+	try {
+   	  // calling the smsManager
+      SmsManager smsManager = SmsManager.getDefault();
+      //inputting parameters into the smsManager
+      smsManager.sendTextMessage(phone, null, message, null, null);
+     //prints SMS sent if the message is successfully send
+      Toast.makeText(getApplicationContext(), "SMS sent.",
+      Toast.LENGTH_LONG).show();
+     } catch (Exception e) {
+   	  //prints failed if it does not send
+        Toast.makeText(getApplicationContext(),
+        "SMS failed, please try again.",
+        Toast.LENGTH_LONG).show();
+        e.printStackTrace();
+     }
+}
 
 	public void goHome(View view) {
 		setContentView(R.layout.add_tasks);
@@ -217,5 +263,3 @@ import com.parse.*;
 
 	}
 }
-
-//http://stackoverflow.com/questions/14376807/how-to-read-write-string-from-a-file-in-android
